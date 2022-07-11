c48e1c4ccec6c00ea260bb7bcf7576158ef3f0de
takkamu <t3017901@gmail.com>
2022-07-11 10:45:55 +0000

    device機能実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "22e0f946b4194d1fa8e4cb26cb9c843a.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
