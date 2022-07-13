e9fafc29e65e622c82e376680ab44bf622951745
takkamu <t3017901@gmail.com>
2022-07-13 14:17:49 +0000

    管理人側会員ページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,16 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
+  namespace :admin do
+    resources :customers, only: [:index, :edit, :show, :update]
   end
-  namespace :public do
+
+  scope module: :public do
+    root to: "homes#top"
     resources :ramens, only: [:new, :index, :edit, :create]
     resources :customers, only: [:index, :edit, :show, :update]
     get 'customers/:id/unsubscribe' => 'customers#unsubscribe'
     patch 'customers/:id/withdraw' => 'customers#withdraw'
   end
 
-  namespace :public do
-    root to: "homes#top"
-  end
   # 会員用
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
